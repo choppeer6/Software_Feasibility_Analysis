3dfdc412ecfd72b56ee65f3efe9220e7f473302e
choppeer6 <1627679280@qq.com>
2025-12-19 21:23:15 +0800

    更新应用配置，修改运行端口为5000；新增数据库数据处理逻辑和数据列表获取功能

--- app.py
+++ app.py
@@ -1209,4 +1209,4 @@ if __name__ == '__main__':
     if not os.path.exists('static'):
         os.makedirs('static')
     
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
